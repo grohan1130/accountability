f9f3cf4a3a339580aef95e32ce5d65a14ce319fb
Rohan <grohan1130@gmail.com>
2026-08-21 00:34:20 -0400

    Registration useState

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,14 +1,38 @@
+import { useState, type ChangeEvent } from "react";
+
 function Register() {
 
+    const [username, setUsername] = useState("")
+    const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setUsername(event.target.value)
+    }
+
+    const [password, setPassword] = useState("")
+    const handlePasswordChange = (event:ChangeEvent<HTMLInputElement>): void => {
+        setPassword(event.target.value)
+    }
+
     const sendCreds = (): void => {
-        console.log('Creds Passed!');
+        console.log(`Creds Passed! Username = ${username} Password = ${password}`);
     };
 
     return (
         <>
             <h1>Create Account</h1>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+
+            <input 
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange = {handleUsernameChange}
+            />
+
+            <input 
+                type="password" 
+                placeholder="Password" 
+                value={password}
+                onChange = {handlePasswordChange}
+            />
             <button id = "submitCreds" onClick={sendCreds}>Submit</button>
         </>
     )
